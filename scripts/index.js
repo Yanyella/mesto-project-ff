141e98b1f50db6e493d8e9171873941be5fb862d
Yana Bakanova <yanysuaBakan@yandex.ru>
2025-01-04 21:19:32 +0300

    исправлены ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -10,48 +10,33 @@ const buttonAddCard = document.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
 
-function createCard(item) {
+function createCard(item, functionDelCard) {
+
     const card = templateCard.querySelector('.card').cloneNode(true);
     card.querySelector('.card__image').src = item.link;
     card.querySelector('.card__image').alt = item.name;
     card.querySelector('.card__title').textContent = item.name;
-    card.querySelector('.card__delete-button');
-    cardList.append(card);
+    const buttonDelCard = card.querySelector('.card__delete-button');
 
-    // вызов функции удаления карточек
+    buttonDelCard.addEventListener('click', function() {
+        functionDelCard(card);
+    });
 
-    const buttonsDelCard = document.querySelectorAll('.card__delete-button');
-    const cards = document.querySelectorAll('.card');
-
-    removeCard(buttonsDelCard, cards);
+    return card;
 }
 
 // @todo: Функция удаления карточки
 
-function removeCard(arrBtn, arr) {
-    for(let i = 0; i < arr.length; i++) {
-        for(let k = 0; k < arrBtn.length; k++) {
-            arrBtn[i].addEventListener('click', function() {
-                arr[i].remove();
-            })
-        }
-    }
+function deleteCard(el) {
+    el.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
-function showCards(arr) {
-    arr.forEach(el => createCard(el));
+for(let i = 0; i < initialCards.length; i++) {
+    const element = createCard(initialCards[i], deleteCard);
+    cardList.append(element);
 }
 
-showCards(initialCards);
 
-// Второй вариант: вывод на страницу картчек при нажатии кнопки "+"
 
-/* function showCards(btn, arr) {
-    btn.addEventListener('click', function() {
-        arr.forEach(el => createCard(el));
-    })
-}
-
-showCards(buttonAddCard, initialCards); */
